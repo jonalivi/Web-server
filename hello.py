81f577ebedd9d389dc65e06d00bd1e0fb1062d10
Jonalivi <jonalivi@users.noreply.github.com>
2016-03-10 01:07:20 +0300

    Hello.py script responses with all the parameters in the QUERY_STRING

---
+++ hello.py
@@ -0,0 +1,11 @@
+def app(env, start_response):
+	status = '200 OK'
+	query_string = env['QUERY_STRING']
+	headers = [
+		('Content-Type', 'text-plain')
+	]
+	args = query_string.replace('&','\n')
+	if not args.endswith('\n'):
+		args = args + '\n'
+	start_response(status, headers)
+	return [args]
