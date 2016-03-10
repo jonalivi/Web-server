154cfb2c08587a5b6cdd359285c965bcf346c781
Jonalivi <jonalivi@users.noreply.github.com>
2016-03-10 03:02:53 +0300

    Content-Type typo fixed

--- hello.py
+++ hello.py
@@ -2,7 +2,7 @@ def app(env, start_response):
 	status = '200 OK'
 	query_string = env['QUERY_STRING']
 	headers = [
-		('Content-Type', 'text-plain')
+		('Content-Type', 'text/plain')
 	]
 	args = query_string.replace('&','\n')
 	if not args.endswith('\n'):
