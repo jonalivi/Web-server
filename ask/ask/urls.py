47f05062014b1e5e241ef6e26f6fbaefa0c40f7e
Jonalivi <jonalivi@users.noreply.github.com>
2016-03-14 19:29:38 +0300

    Basic router for Ask application

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -14,8 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.conf.urls import url
-from django.contrib import admin
+from . import views
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+	url(r'^$', views.test),
+	url(r'^login/', views.test),
+	url(r'^signup/', views.test),
+	url(r'^question/(?P<question_id>[^/]+)/', views.test),
+	url(r'^ask/', views.test),
+	url(r'^popular/', views.test),
+	url(r'^new/', views.test),
 ]
