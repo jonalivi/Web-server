07d150e974236e03e5b96e4b9b0afde57fb2db06
Jonalivi <jonalivi@users.noreply.github.com>
2016-03-16 01:36:46 +0300

    Question and Answer models

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -1,5 +1,24 @@
 from __future__ import unicode_literals
 
-from django.db import models
+from django.contrib.auth.models import User
+from django.db import models 
 
-# Create your models here.
+class Question(models.Model):
+	title = models.CharField(max_length=255)
+	text = models.TextField()
+	added_at = models.DateTimeField()
+	rating = models.IntegerField()
+	author = User()
+	likes = models.ManyToManyField(User)
+
+	def __str__(self):
+		return '%s : %s : %s' % (self.author, self.title, self.text)
+
+class Answer(models.Model):
+	text = models.TextField()
+	added_at = models.DateTimeField()
+	question = models.OneToOneField(Question)
+	author = User()
+
+	def __str__(self):
+		return '%s : Re:%s : %s' % (self.author, self.question, self.text)
