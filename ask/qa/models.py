81a6529de4290c4e8b7e117d6e1e79ebe60106cb
Jonalivi <jonalivi@users.noreply.github.com>
2016-03-16 23:27:36 +0300

    Models of Question and Answer fixed: ForeignKey

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -1,23 +1,27 @@
 from __future__ import unicode_literals
 
+from django.core.urlresolvers import reverse
 from django.contrib.auth.models import User
 from django.db import models 
 
 class Question(models.Model):
 	title = models.CharField(max_length=255)
 	text = models.TextField()
-	added_at = models.DateTimeField()
-	rating = models.IntegerField()
+	added_at = models.DateTimeField(blank=True)
+	rating = models.IntegerField(blank=True)
 	author = User()
 	likes = models.ManyToManyField(User)
 
 	def __str__(self):
 		return '%s : %s : %s' % (self.author, self.title, self.text)
 
+	def get_url(self):
+		return '/question/%d' % self.pk
+
 class Answer(models.Model):
 	text = models.TextField()
 	added_at = models.DateTimeField()
-	question = models.OneToOneField(Question)
+	question = models.ForeignKey(Question, null=True, on_delete=models.SET_NULL)
 	author = User()
 
 	def __str__(self):
