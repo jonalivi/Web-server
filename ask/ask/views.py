b51540f6b543cbccf7cc2b82b03d2ef1ebfe8823
Jonalivi <jonalivi@users.noreply.github.com>
2016-03-16 23:30:57 +0300

    Controller for new, popular and single questions

--- ask/ask/views.py
+++ ask/ask/views.py
@@ -1,4 +1,62 @@
-from django.http import HttpResponse
+from django.http import HttpRequest, HttpResponse, Http404
+from django.core.paginator import Paginator, Page, EmptyPage
+from django.shortcuts import render
+from django.views.decorators.http import require_GET
+from qa.models import Question, Answer
 
 def test(request, *args, **kwargs):
 	return HttpResponse('OK')
+
+@require_GET
+def questions_main(request):
+	questions = Question.objects.all()
+	questions = questions.order_by('-pk')
+	try:
+		page = int(request.GET.get('page', 1))
+	except ValueError:
+		raise Http404
+	paginator = Paginator(questions, 10)
+	paginator.baseurl = '/?page='
+	try:
+		page = paginator.page(page)
+	except EmptyPage:
+		raise Http404
+	return render(request, 'questions_all.html', {
+		'questions':	page.object_list,
+		'paginator':	paginator,
+		'page':		page,
+	})
+
+@require_GET
+def questions_popular(request):
+	questions = Question.objects.all()
+	questions = questions.order_by('-rating', '-pk')
+	try:
+		page = int(request.GET.get('page', 1))
+	except ValueError:
+		raise Http404
+	paginator = Paginator(questions, 10)
+	paginator.baseurl = '/popular/?page='
+	try:
+		page = paginator.page(page)
+	except EmptyPage:
+		raise Http404
+	return render(request, 'questions_popular.html', {
+		'questions':	page.object_list,
+		'paginator':	paginator,
+		'page':		page,
+	})
+	
+	
+@require_GET
+def question_single(request, question_id=None):
+	if not question_id:
+		raise Http404
+	q = Question.objects.filter(pk=question_id)[0]
+	if not q:
+		raise Http404
+	answers = Answer.objects.filter(question=q).order_by('-pk')
+	return render(request, 'question.html', {
+		'question':	q,
+		'answers':	answers,
+	})		 
