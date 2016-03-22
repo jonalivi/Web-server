f44f7bfb93bbe11c85bc4dd40a2bd55495e6ce1b
Jonalivi <jonalivi@users.noreply.github.com>
2016-03-22 16:01:33 +0300

    trivial

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -4,25 +4,27 @@ from django.core.urlresolvers import reverse
 from django.contrib.auth.models import User
 from django.db import models 
 
+from django.utils import timezone
+
 class Question(models.Model):
 	title = models.CharField(max_length=255)
 	text = models.TextField()
-	added_at = models.DateTimeField(blank=True)
+	added_at = models.DateTimeField(default=timezone.now())
 	rating = models.IntegerField(blank=True)
-	author = models.CharField(max_length=30)
+	author = models.CharField(max_length=30, default='anon')
 	likes = models.ManyToManyField(User)
 
 	def __str__(self):
 		return '%s : %s : %s' % (self.author, self.title, self.text)
 
 	def get_url(self):
-		return '/question/%d' % self.pk
+		return '/question/%d/' % self.pk
 
 class Answer(models.Model):
 	text = models.TextField()
-	added_at = models.DateTimeField()
-	question = models.ForeignKey(Question, null=True, on_delete=models.SET_NULL)
-	author = models.CharField(max_length=30)
+	added_at = models.DateTimeField(default=timezone.now())
+	question = models.ForeignKey(Question, on_delete=models.CASCADE)
+	author = models.CharField(max_length=30, default='anon')
 
 	def __str__(self):
 		return '%s : Re:%s : %s' % (self.author, self.question, self.text)
